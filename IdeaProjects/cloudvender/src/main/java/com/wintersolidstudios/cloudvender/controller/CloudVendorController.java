d34d1b98425ac6212d2b97521125723f23db1bab
wintersolid <ztanksley@luc.edu>
2024-06-15 11:19:57 -0700

    Mapping and commenting.

--- IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvender/controller/CloudVendorController.java
+++ IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvender/controller/CloudVendorController.java
@@ -1,8 +1,14 @@
 package com.wintersolidstudios.cloudvender.controller;
-
+import com.wintersolidstudios.cloudvender.service.CloudVendorService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 
 @RestController
@@ -19,6 +25,57 @@ public class CloudVendorController {
         this.cloudVendorService = cloudVendorService;
     }
 
+    /**
+     * Get details of a specific cloud vendor by ID.
+     */
+    @GetMapping("/{vendorId}")
+    @ApiOperation(value = "Get a cloud vendor by Id", response = ResponseEntity.class)
+    public ResponseEntity<Object> getCloudVendorDetails(
+            @ApiParam(value = "ID of the cloud vendor to retrieve", required = true) @PathVariable("vendorId") String vendorId) {
+        return ResponseHandler.responseBuilder("Requested Vendor Details are given here",
+                HttpStatus.OK, cloudVendorService.getCloudVendor(vendorId));
+    }
 
+    /**
+     * Get details of all cloud vendors.
+     */
+    @GetMapping("/")
+    @ApiOperation(value = "View a list of available cloud vendors", response = List.class)
+    public List<CloudVendor> getAllCloudVendorDetails() {
+        return cloudVendorService.getAllCloudVendors();
+    }
 
+    /**
+     * Create a new cloud vendor.
+     */
+    @PostMapping("/")
+    @ApiOperation(value = "Create a new cloud vendor")
+    public String createCloudVendorDetails(
+            @ApiParam(value = "Cloud Vendor object to be created", required = true) @Valid @RequestBody CloudVendor cloudVendor) {
+        cloudVendorService.createCloudVendor(cloudVendor);
+        return "Cloud Vendor Created Successfully";
+    }
+
+    /**
+     * Update an existing cloud vendor's details.
+     */
+    @PutMapping("/")
+    @ApiOperation(value = "Update an existing cloud vendor")
+    public String updateCloudVendorDetails(
+            @ApiParam(value = "Cloud Vendor object with updated details", required = true) @Valid @RequestBody CloudVendor cloudVendor) {
+        cloudVendorService.updateCloudVendor(cloudVendor);
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    /**
+     * Delete a cloud vendor by ID.
+     */
+    @DeleteMapping("/{vendorId}")
+    @ApiOperation(value = "Delete a cloud vendor")
+    public String deleteCloudVendorDetails(
+            @ApiParam(value = "ID of the cloud vendor to be deleted", required = true) @PathVariable("vendorId") String vendorId) {
+        cloudVendorService.deleteCloudVendor(vendorId);
+        return "Cloud Vendor Deleted Successfully";
+    }
 }
+
