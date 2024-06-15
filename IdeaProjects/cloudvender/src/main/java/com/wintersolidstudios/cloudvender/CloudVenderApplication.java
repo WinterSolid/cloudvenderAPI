797a0763a79075310f347eda764884eac021343d
wintersolid <ztanksley@luc.edu>
2024-06-15 10:00:32 -0700

    Added Application And POM file

---
+++ IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvender/CloudVenderApplication.java
@@ -0,0 +1,13 @@
+package com.wintersolidstudios.cloudvender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudVenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudVenderApplication.class, args);
+	}
+
+}
