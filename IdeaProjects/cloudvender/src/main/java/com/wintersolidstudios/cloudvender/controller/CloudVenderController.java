661e71b8f9f47e1cf4f090866811787dad8dbec9
wintersolid <ztanksley@luc.edu>
2024-06-15 10:23:17 -0700

    Constructor injection for CloudVendorService

--- IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvender/controller/CloudVenderController.java
+++ IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvender/controller/CloudVenderController.java
@@ -2,15 +2,9 @@ package com.wintersolidstudios.cloudvender.controller;
 
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/cloudvendor")
