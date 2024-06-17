affe442fce1e8e72b33069334ae189a77e79fd00
wintersolid <ztanksley@luc.edu>
2024-06-17 14:46:56 -0700

    Update dependency version due to vulnerability, update to CloudVendorExceptionHandler,

--- IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvendor/exception/CloudVendorNotFoundException.java
+++ IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvendor/exception/CloudVendorNotFoundException.java
@@ -1,4 +1,12 @@
 package com.wintersolidstudios.cloudvendor.exception;
 
-public class CloudVendorNotFoundException {
+public class CloudVendorNotFoundException extends RuntimeException {
+
+    public CloudVendorNotFoundException(String message) {
+        super(message);
+    }
+
+    public CloudVendorNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
