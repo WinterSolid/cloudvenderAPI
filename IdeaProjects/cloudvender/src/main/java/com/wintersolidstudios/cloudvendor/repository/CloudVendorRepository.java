f8d9af2d3235d2288d380838aeb045416419f7dd
wintersolid <ztanksley@luc.edu>
2024-06-17 13:56:33 -0700

    Create CloudVendorRepository added dependency to maven

---
+++ IdeaProjects/cloudvender/src/main/java/com/wintersolidstudios/cloudvendor/repository/CloudVendorRepository.java
@@ -0,0 +1,11 @@
+package com.wintersolidstudios.cloudvendor.repository;
+
+
+import com.wintersolidstudios.cloudvendor.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+    List<CloudVendor> findByVendorName(String vendorName);
+}
